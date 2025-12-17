f6e1457d59aecfea0fc25885c98d0a60cbabce01
Gaurav <gaurav.yadav@caremonitor.com.au>
2025-12-17 14:01:39 +0530

    minor

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,62 +18,74 @@ class LinkifyExample extends StatelessWidget {
       title: 'linkify_plus example',
       home: Scaffold(
         appBar: AppBar(title: const Text('linkify_plus example')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Center(
-              child: Linkify(
-                onOpen: _onOpen,
-                textScaleFactor: 2,
-                options: const LinkifyOptions(),
-                text:
-                    "Hi Jane,Please be advised your team has added a new educational resource \n#https://preprod.caremonitor.com.au/links/?c=UI0b5V0#Falls prevention – eyesight# to your resource library. ",
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Center(
+                child: Linkify(
+                  onOpen: _onOpen,
+                  textScaleFactor: 2,
+                  options: const LinkifyOptions(),
+                  text:
+                      "Hi Jane,Please be advised your team has added a new educational resource \n#https://preprod.caremonitor.com.au/links/?c=UI0b5V0#Falls prevention – eyesight# to your resource library. ",
+                ),
               ),
-            ),
-            Center(
-              child: Linkify(
-                onOpen: _onOpen,
-                textScaleFactor: 2,
-                options: const LinkifyOptions(),
-                text: "Made by #https://cretezy.com#falls#",
+              Center(
+                child: Linkify(
+                  onOpen: _onOpen,
+                  textScaleFactor: 2,
+                  options: const LinkifyOptions(),
+                  text: "Made by #https://cretezy.com#falls#",
+                ),
               ),
-            ),
-            Center(
-              child: Linkify(
-                onOpen: _onOpen,
-                linkifiers: const [HyperLinkifier()],
-                textScaleFactor: 2,
-                options: const LinkifyOptions(),
-                text: "[Test](http://urltolinkto.com)",
+              Center(
+                child: Linkify(
+                  onOpen: _onOpen,
+                  linkifiers: const [HyperLinkifier()],
+                  textScaleFactor: 2,
+                  options: const LinkifyOptions(),
+                  text: "[Test](http://urltolinkto.com)",
+                ),
               ),
-            ),
-            Center(
-              child: Linkify(
-                onOpen: _onOpen,
-                linkifiers: const [HyperLinkifier()],
-                textScaleFactor: 2,
-                options: const LinkifyOptions(),
-                text: "[Mail](tel:123-456-7890)",
+              Center(
+                child: Linkify(
+                  onOpen: _onOpen,
+                  linkifiers: const [HyperLinkifier()],
+                  textScaleFactor: 2,
+                  options: const LinkifyOptions(),
+                  text: "[Mail](tel:123-456-7890)",
+                ),
               ),
-            ),
-            Center(
-              child: Linkify(
-                onOpen: _onOpen,
-                textScaleFactor: 2,
-                options: const LinkifyOptions(),
-                text:
-                    "Made by #https://dev.ihealthlabs.com/account/sign-up-success#iHealth# and unMade by #https://dev.google.com/account/sign-up-success#iHealth3# and unMade by https://dev.google.com/account/sign-up-success",
+              Center(
+                child: Linkify(
+                  onOpen: _onOpen,
+                  textScaleFactor: 2,
+                  options: const LinkifyOptions(),
+                  text:
+                      "Made by #https://dev.ihealthlabs.com/account/sign-up-success#iHealth# and unMade by #https://dev.google.com/account/sign-up-success#iHealth3# and unMade by https://dev.google.com/account/sign-up-success",
+                ),
               ),
-            ),
-            Center(
-              child: SelectableLinkify(
-                onOpen: _onOpen,
-                linkifiers: const [UserTagLinkifier()],
-                textScaleFactor: 4,
-                text: 'Hello @JohnDoe, did you see what @JaneDoe posted?',
+              Center(
+                child: Linkify(
+                    onOpen: _onOpen,
+                    textScaleFactor: 2,
+                    options: const LinkifyOptions(),
+                    text:
+                        "Please find a link to an educational resource below.\n#https://stg.caremonitor.com.au/links/?c=V5cP5P5#Controlling fluid intake in heart failure#,\n#https://stg.caremonitor.com.au/links/?c=V5cP5N0#9 food and heart health myths#,\n#https://stg.caremonitor.com.au/links/?c=V5cP5Ok#Common investigations in cardiovascular disease#,\n#https://stg.caremonitor.com.au/links/?c=V5cP5NO#Anticoagulants & blood thinners#,\n#https://stg.caremonitor.com.au/links/?c=V5cP5Nj#Antiplatelet treatment#,\n#https://stg.caremonitor.com.au/links/?c=V5cP5O4#Cardiac rehabilitation videos#,\n#https://stg.caremonitor.com.au/links/?c=V5cP5OP#Client Services Charter#\n\nKind Regards,\nYour virtual care team \n"),
               ),
-            ),
-          ],
+
+              //
+              Center(
+                child: SelectableLinkify(
+                  onOpen: _onOpen,
+                  linkifiers: const [UserTagLinkifier()],
+                  textScaleFactor: 4,
+                  text: 'Hello @JohnDoe, did you see what @JaneDoe posted?',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
